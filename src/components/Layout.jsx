2491c3fa1fa03f40248fbf7f28398c086c64f05d
alvaroormeno <alvaro@ormeno.org>
2022-07-26 22:57:47 -0400

    basic layout navbar, complete

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Layout = () => {
   return (
-    <div>Layout</div>
+    
+    <main className='h-screen w-screen bg-gradient-to-b from-violet-500 to-fuchsia-500 ' >
+
+        {/* NAVBAR */}
+        <div className='flex flex-row h-[120px] justify-center items-center text-4xl font-[800] text-white '>
+            Top US News Now ! 
+        </div>
+
+    </main>
+
+
+
   )
 }
 
