41fb7728f7adb0ce28ad0e25bfbb05848522f611
alvaroormeno <alvaro@ormeno.org>
2022-07-27 00:08:15 -0400

    globalcontext.js complete, need to test

--- src/GlobalContext.js
+++ src/GlobalContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
 import axios from 'axios';
 
 
@@ -7,14 +7,21 @@ const GlobalContext = createContext()
 
 const GlobalProvider = () => {
 
-    // State to save data after fetching
-    const [data, setData] = useState();
+    // State to save news data after fetching
+    const [newsData, setNewsData] = useState();
     // Api Key 
     const apiKey = "dfcc844fab394ee09fbbe0ad122029c7"
+    // useEffect hook to call api every reload
+    useEffect(() => {axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`)
+        .then(res => setNewsData(res.data))
+        .catch(err => console.log(err))
+    }, []);
 
   return (
-    <div>GlobalContext</div>
+    <GlobalContext.Provider value={{newsData}}>
+        {props.children}
+    </GlobalContext.Provider>
   )
 }
 
-export default GlobalProvider
+export {GlobalProvider, GlobalContext}
