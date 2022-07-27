453b64f3475baeb5eecb4abd4fe8d43c02fc3d1a
alvaroormeno <alvaro@ormeno.org>
2022-07-26 23:43:23 -0400

    declared globalcontext, globalprovider, data state and apikey

--- src/GlobalContext.js
+++ src/GlobalContext.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { createContext, useContext } from 'react'
+import axios from 'axios';
 
-const GlobalContext = () => {
+
+// Create Context
+const GlobalContext = createContext()
+
+const GlobalProvider = () => {
+
+    // State to save data after fetching
+    const [data, setData] = useState();
+    // Api Key 
+    const apiKey = "dfcc844fab394ee09fbbe0ad122029c7"
+
   return (
     <div>GlobalContext</div>
   )
 }
 
-export default GlobalContext
+export default GlobalProvider
